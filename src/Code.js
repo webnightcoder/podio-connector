f1b4816296b05243e2bfdd1fdcb478d0d7e1ced3
bajarang-agarwal <bajarang@lucentasolutions.com>
2019-02-27 12:45:33 +0530

    add logger

--- src/Code.js
+++ src/Code.js
@@ -4,6 +4,8 @@ var LOGGER         = require('./services/logger.js');
 
 LOGGER.setLogger(CONF);
 
+LOGGER.log("Starting App.");
+
 function getConnector(){
   return new connectorSvc({
     CacheService: CacheService,
@@ -19,53 +21,41 @@ function getConfig() {
   return getConnector().getConfig();
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
+
 function getSchema() {
   return getConnector().getSchema();
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
+
 function getData(request) {
   LOGGER.log('REQUEST:', request);
   return getConnector().getData(request);
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
+
 function getAuthType() {
 
   LOGGER.log("Auth Type is  : " + getConnector().getAuthType() );
   return getConnector().getAuthType();
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
 function isAdminUser() {
   return getConnector().isAdminUser();
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
 function authCallback(request) {
   return getConnector().authCallback(request);
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
 function isAuthValid() {
   return getConnector().isAuthValid();
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
 function resetAuth() {
   getConnector().resetAuth();
 }
 
-/* istanbul ignore next */
-// eslint-disable-next-line no-unused-vars
+
 function get3PAuthorizationUrls() {
   return getConnector().get3PAuthorizationUrls();
 }
